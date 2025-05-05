f06884c34a2f785914d14ac846089e084730708a
Jesse L. Zamora <xtremekforever@gmail.com>
2025-05-05 19:47:01 -0400

    Add initial project for NRF52840 with LED0 blinking example

---
+++ src/BridgingHeader.h
@@ -0,0 +1,13 @@
+#include <autoconf.h>
+#include <zephyr/kernel.h>
+#include <zephyr/drivers/gpio.h>
+
+#include <zephyr/bluetooth/bluetooth.h>
+#include <zephyr/bluetooth/gatt.h>
+#include <zephyr/bluetooth/hci.h>
+
+// See [struct bt_conn issue, how to include conn_internal.h - Nordic Q&A - Nordic DevZone - Nordic DevZone](https://devzone.nordicsemi.com/f/nordic-q-a/88467/struct-bt_conn-issue-how-to-include-conn_internal-h)
+#include <../subsys/bluetooth/host/conn_internal.h>
+
+#define LED0_NODE DT_ALIAS(led0)
+static struct gpio_dt_spec led0 = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
---
+++ src/Main.swift
@@ -0,0 +1,37 @@
+@main
+struct Main {
+    static func main() {
+        let led0Handle = Led(gpio: &led0)
+
+        while true {
+            led0Handle.toggle()
+
+            k_msleep(100)
+        }
+    }
+}
+
+struct Led {
+    let gpio: UnsafePointer<gpio_dt_spec>
+
+    init(gpio: UnsafePointer<gpio_dt_spec>) {
+        self.gpio = gpio
+
+        guard gpio_is_ready_dt(gpio) else { return }
+
+        let ret = gpio_pin_configure_dt(gpio, GPIO_OUTPUT | GPIO_OUTPUT_INIT_LOW | GPIO_OUTPUT_INIT_LOGICAL)
+        guard ret == 0 else { return }
+    }
+
+    func on() {
+        gpio_pin_set_dt(gpio, 1)
+    }
+
+    func off() {
+        gpio_pin_set_dt(gpio, 0)
+    }
+
+    func toggle() {
+        gpio_pin_toggle_dt(gpio)
+    }
+}
