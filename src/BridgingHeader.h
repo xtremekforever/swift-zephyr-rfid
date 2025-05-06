d84bb10a41595cb0e121b9fa96f9d60700d66a92
Jesse L. Zamora <xtremekforever@gmail.com>
2025-05-06 14:41:10 -0400

    Add buzzer output on Adafruit P0.28

--- src/BridgingHeader.h
+++ src/BridgingHeader.h
@@ -11,3 +11,6 @@
 
 #define LED0_NODE DT_ALIAS(led0)
 static struct gpio_dt_spec led0 = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
+
+#define BUZZER_NODE DT_ALIAS(buzzer)
+static struct gpio_dt_spec buzzer = GPIO_DT_SPEC_GET(BUZZER_NODE, gpios);
--- src/Main.swift
+++ src/Main.swift
@@ -2,6 +2,7 @@
 struct Main {
     static func main() {
         let led0Handle = Led(gpio: &led0)
+        let buzzerHandle = Led(gpio: &buzzer)
 
         while true {
             led0Handle.toggle()
